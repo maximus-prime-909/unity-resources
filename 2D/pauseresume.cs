04511a316397ee8c4e8b3ba8066001803293a7e9
Manmeet Kumar <66664358+manmeetkr19@users.noreply.github.com>
2021-09-30 23:47:50 +0530

    Script for pausing and resuming from UI

---
+++ 2D/pauseresume.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseresume : MonoBehaviour
+{
+    // Start is called before the first frame 
+    [SerializeField] GameObject canvas;
+    [SerializeField] GameObject pauseButton;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        { 
+            Time.timeScale = 0;
+            resumemenu();
+        }
+    }
+   public void pause()
+    {
+        pauseButton.SetActive(false);
+         Time.timeScale = 0;
+        resumemenu();
+    }
+    void resumemenu()
+    {
+        canvas.SetActive(true);
+    }
+
+   public void resume()
+    {
+        canvas.SetActive(false);
+        Time.timeScale = 1;
+        pauseButton.SetActive(true);
+
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
