728c07b59290c9ceaee04916cdb6342640d2e2d5
Nirlov Deb <66939373+Nirlov24@users.noreply.github.com>
2021-10-01 09:15:43 +0530

    2D drone Controls

---
+++ 2D/Drone_Controls.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Drone_Controls : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 100f; //for rotation
+    [SerializeField] float mainThrust = 100f; //for thrust
+    [SerializeField] float levelLoadDelay = 2f;
+
+   [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
+
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    enum State {  Alive, Dying, Transcending }
+    State state = State.Alive;
+
+    bool collisionsDisabled= false; //for debug
+    
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+        //todo somewhere stop sound on death
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+
+        if (Debug.isDebugBuild)
+        {
+            RespondToDebugKeys();   //todo only if debug on i.e commented
+        }    
+    }
+
+
+    private void RespondToDebugKeys() // only if debug 
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if(Input.GetKeyDown(KeyCode.C))
+        {
+            collisionsDisabled = !collisionsDisabled;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive || collisionsDisabled) // // only for debug
+        {
+            return;
+        }
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+               
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                //kill player
+                StartDeathSequence();
+                break;
+        }
+    }
+
+   
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticles.Play();
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+
+    private void LoadNextLevel() //Level loop
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0; //loop back to start
+        }
+         SceneManager.LoadScene(nextSceneIndex); 
+        
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0); 
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+    }
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+            mainEngineParticles.Play();
+        }
+    }
+    
+    
+    private void RespondToRotateInput()
+    {
+
+        rigidBody.freezeRotation = true; // take manual control of rotation
+       
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A)) //"A" takes preference on "D" when pressed together as "A" is written first
+        {
+           
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rigidBody.freezeRotation = false; // resume physics control of rotation
+    }
+    
+}
