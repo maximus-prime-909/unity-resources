72afed13920fd0495d76f57491b985b79406921d
KonH <konh@yandex.ru>
2021-10-12 20:26:53 +0700

    Code cleanup
    Remove unused using directives, add missing new lines, remove unused empty method

--- 2D/pauseresume.cs
+++ 2D/pauseresume.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class pauseresume : MonoBehaviour
 {
     // Start is called before the first frame 
     [SerializeField] GameObject canvas;
     [SerializeField] GameObject pauseButton;
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
--- 3D/MainMenu.cs
+++ 3D/MainMenu.cs
@@ -1,32 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
     public void setVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
     }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
     }
+
     public void QuitGame()
     {
         Application.Quit();
     }
+
     public void ReturnMM()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
+
     public void CreditsScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
+
     public void setQuality(int qIndex)
     {
         QualitySettings.SetQualityLevel(qIndex);
